f2f3221691725b0ad2ab439017cfb6c753fff6c1
samharshe <samharshe29@gmail.com>
2023-03-14 00:47:46 +0000

    did all the simulations we wanted to and got out the answer that Lagos and Oleg calculated explicitly soooo this has been quite a success

--- script3.py
+++ script3.py
@@ -8,10 +8,11 @@ def calculate_new_pot(old_pot, bet, weight):
 outputs = open('outputs2.txt', 'w')
 
 weights = np.arange(0.5, 1.0, 0.05).tolist()
-bets = np.arange(0.0, 1.0, 0.005).tolist()
-average_rounds = []
+bets = np.arange(0.005, 1.0, 0.005).tolist()
+least_rounds = []
 
 for weight in weights:
+    average_rounds = []
     for bet in bets:
         rounds = []
         for i in range(100):
@@ -20,12 +21,23 @@ for weight in weights:
                 if pot >= 2:
                     rounds.append(j)
                     break
+                elif pot <= 0.01:
+                    rounds.append(10000)
+                    break
                 else: 
                     pot = calculate_new_pot(pot, bet, weight)
             if pot < 2:
                 rounds.append(10000)
         average_rounds.append(sum(rounds)/len(rounds))
-
-    plt.scatter(bets, averrage_rounds)
+    
+    least_rounds.append(bets[average_rounds.index(min(average_rounds))])
+    
+    plt.scatter(bets, average_rounds)
     plt.title(f"Bet size v. average number of rounds to double for coin weight {round(weight, 2)}")
     plt.show()
+
+plt.scatter(weights, least_rounds)
+x, y = [0.5, 1], [0, 1]
+plt.plot(x, y, marker = 'o')
+plt.title(f"Coin weight v. the bet size at which the pot took the least number of rounds to double")
+plt.show()
